7f8836a370ecad8bfe3b8692eab9820314a7d9e7
Roman <slyusarrv@yandex.ru>
2023-01-11 17:52:58 +0500

    optimization ORM qyeries

--- service/service/settings.py
+++ service/service/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-p@)vun^w+f35)-&kmuxt*g4t1(!-qq4xg^q&k^tw=_tp!nbsky
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -72,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'service.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -85,7 +82,6 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASS'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -105,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -119,7 +114,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -129,3 +123,17 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Чтобы посмотреть запросы в базу в консоли
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {'class': 'logging.StreamHandler'}
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
+        }
+    }
+}
--- service/services/views.py
+++ service/services/views.py
@@ -1,10 +1,23 @@
+from django.db.models import Prefetch
 from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
+from clients.models import Client
 from services.models import Subscription
 from services.serializers import SubscriptionSerializer
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.all()
+    # Проблема N+1 для клиентов решается prefetch_related, в запросе появляется ..."clients_client"."id" IN (1, 2);
+    # Для email пользователя второй prefetch_related
+    # queryset = Subscription.objects.all().prefetch_related(
+    #     'client').prefetch_related('client__user')
+
+    # Либо пишем при помощи класса, чтобы исключить ненужные поля в запросах (queryset в классе Prefetch)
+    # Теперь не 3 а 2 запроса, пристствует INNER JOIN "auth_user"
+    queryset = Subscription.objects.all().prefetch_related(
+        Prefetch('client', queryset=Client.objects.all()
+                 .select_related('user').only('company_name', 'user__email'))
+    )
+
     serializer_class = SubscriptionSerializer
