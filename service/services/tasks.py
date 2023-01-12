fa18b3c45624123bbbf6bb165adc460e5b30829a
Roman <slyusarrv@yandex.ru>
2023-01-12 17:59:32 +0500

    use annotate in task

--- service/services/tasks.py
+++ service/services/tasks.py
@@ -1,12 +1,15 @@
 from celery import shared_task
+from django.db.models import F
 
 
 @shared_task
 def set_price(subscriprion_id):
     from services.models import Subscription
 
-    subscribtion = Subscription.objects.get(id=subscriprion_id)
-    new_price = (subscribtion.service.full_price -
-                 subscribtion.service.full_price * subscribtion.plan.discount_percent / 100)
-    subscribtion.price = new_price
+    subscribtion = Subscription.objects.filter(id=subscriprion_id).annotate(
+        annotated_price=F('service__full_price') -
+              F('service__full_price') * F('plan__discount_percent') / 100.00).first()
+    # new_price = (subscribtion.service.full_price -
+    #              subscribtion.service.full_price * subscribtion.plan.discount_percent / 100)
+    subscribtion.price = subscribtion.annotated_price
     subscribtion.save()
