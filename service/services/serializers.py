521602f7964706672a18ec51ecf50f22e1b0ec17
Roman <slyusarrv@yandex.ru>
2023-01-12 12:47:13 +0500

    add annotate

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -20,9 +20,10 @@ class SubscriptionSerializer(serializers.ModelSerializer):
     # instance это конкретная модель подписки (Subscription),
     # которую мы обрататываем в этом сериалайзере
     def get_price(self, instance):
-        return (instance.service.full_price
-                - instance.service.full_price
-                * (instance.plan.discount_percent / 100))
+        # return (instance.service.full_price
+        #         - instance.service.full_price
+        #         * (instance.plan.discount_percent / 100))
+        return instance.price
 
     class Meta:
         model = Subscription
