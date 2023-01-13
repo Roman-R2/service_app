3e932d71a4984f22b60600013b4f642f1d0233eb
Roman <slyusarrv@yandex.ru>
2023-01-13 11:20:40 +0500

    solution for problem with asunch run

--- service/services/tasks.py
+++ service/services/tasks.py
@@ -3,6 +3,7 @@ import time
 
 from celery import shared_task
 from celery_singleton import Singleton
+from django.db import transaction
 from django.db.models import F
 
 
@@ -10,27 +11,23 @@ from django.db.models import F
 def set_price(subscriprion_id):
     from services.models import Subscription
 
-    time.sleep(5)
+    with transaction.atomic():
+        subscribtion = Subscription.objects.select_for_update().filter(id=subscriprion_id).annotate(
+            annotated_price=F('service__full_price') -
+                            F('service__full_price') * F('plan__discount_percent') / 100.00).first()
+        # new_price = (subscribtion.service.full_price -
+        #              subscribtion.service.full_price * subscribtion.plan.discount_percent / 100)
 
-    subscribtion = Subscription.objects.filter(id=subscriprion_id).annotate(
-        annotated_price=F('service__full_price') -
-                        F('service__full_price') * F('plan__discount_percent') / 100.00).first()
-    # new_price = (subscribtion.service.full_price -
-    #              subscribtion.service.full_price * subscribtion.plan.discount_percent / 100)
-
-    time.sleep(20)
-
-    subscribtion.price = subscribtion.annotated_price
-    subscribtion.save()
+        subscribtion.price = subscribtion.annotated_price
+        subscribtion.save()
 
 
 @shared_task(base=Singleton)
 def set_comment(subscriprion_id):
     from services.models import Subscription
 
-    subscribtion = Subscription.objects.get(id=subscriprion_id)
+    with transaction.atomic():
+        subscribtion = Subscription.objects.select_for_update().get(id=subscriprion_id)
 
-    time.sleep(27)
-
-    subscribtion.comment = str(datetime.datetime.now())
-    subscribtion.save()
+        subscribtion.comment = str(datetime.datetime.now())
+        subscribtion.save()
