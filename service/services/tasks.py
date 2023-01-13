3d5f12ebeb9e0bc484a509d5037efa9adbac7d7f
Roman <slyusarrv@yandex.ru>
2023-01-13 10:44:07 +0500

    add celery-singleton

--- service/services/tasks.py
+++ service/services/tasks.py
@@ -1,14 +1,15 @@
 from celery import shared_task
+from celery_singleton import Singleton
 from django.db.models import F
 
 
-@shared_task
+@shared_task(base=Singleton)
 def set_price(subscriprion_id):
     from services.models import Subscription
 
     subscribtion = Subscription.objects.filter(id=subscriprion_id).annotate(
         annotated_price=F('service__full_price') -
-              F('service__full_price') * F('plan__discount_percent') / 100.00).first()
+                        F('service__full_price') * F('plan__discount_percent') / 100.00).first()
     # new_price = (subscribtion.service.full_price -
     #              subscribtion.service.full_price * subscribtion.plan.discount_percent / 100)
     subscribtion.price = subscribtion.annotated_price
