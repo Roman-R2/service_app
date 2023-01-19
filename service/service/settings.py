112dea43630c4d4a5e900923db2d38ba6f4e2bc8
Roman <slyusarrv@yandex.ru>
2023-01-19 10:48:25 +0500

    added django-cachalot

--- service/service/settings.py
+++ service/service/settings.py
@@ -29,15 +29,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'clients',
-    'services',
-
+    # Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party apps
+    'cachalot',
+
+    # Our apps
+    'clients',
+    'services',
 ]
 
 MIDDLEWARE = [
